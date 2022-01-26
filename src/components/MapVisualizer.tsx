d8635964c75d41e4c891421ca72dd7a3f7bef4fd
wiwium <ketanchaudhari1998@gmail.com>
2022-01-26 21:46:08 +0530

    D3 - version upgrade to 7 and split d3 into smaller imports

--- src/components/MapVisualizer.tsx
+++ src/components/MapVisualizer.tsx
@@ -1,68 +1,67 @@
-// 😫😫Please ignore the typescript ignore-statements 😫😫 Just want to get d3 working first.
-// 😁😁 After d3 is properly setup i'll worry about typescript.
-
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React, { useRef, useEffect } from 'react';
 import useSWR from 'swr';
-import * as d3 from 'd3';
-import * as topojson from 'topojson';
+import { select } from 'd3-selection';
+import { json } from 'd3-fetch';
+import { feature } from 'topojson-client';
+import { geoMercator, geoPath } from 'd3-geo';
+// eslint-disable-next-line import/no-unresolved
+import { Topology, GeometryCollection } from 'topojson-specification';
 import { Data } from '../types/Types';
 import { BASE_URL, STATE_CODES, MAP_DIMENSIONS } from '../Constants';
 
-// Reference Commit: https://github.com/covid19india/covid19india-react/blob/9ef4ca39173a58859996d4d8151a64c385533ca9/src/components/mapvisualizer.js
-const MapVisualizer = () => {
-  // move data loading to main component
-  // https://data.covid19india.org/documentation/v4_data.html
+type GeometryProperties = {
+  st_nm: string;
+  st_code: string;
+  district?: string;
+};
+
+type TopologyObject = {
+  states: GeometryCollection<GeometryProperties>;
+};
+
+const MapVisualizer: React.FC = () => {
   const { data } = useSWR<Data>(
     'https://data.covid19india.org/v4/min/data.min.json',
-    (link) => fetch(link).then((res) => res.json()), // we can generate uniques 'key' here and append it to the data.
+    (link) => fetch(link).then((res) => res.json()),
     {
       revalidateOnMount: true,
       suspense: true, // fallback to top-most suspense till load
-      // also as suspense data will never be null -> https://swr.vercel.app/docs/suspense
     }
   );
-  const { data: geoData } = useSWR(
+
+  const { data: geoData } = useSWR<Topology<TopologyObject>>(
     `${BASE_URL}api/india.json`,
-    (link) => d3.json(link), // we can generate uniques 'key' here and append it to the data.
+    (link): Promise<any> => json(link), // we can generate uniques 'key' here and append it to the data.
     {
       revalidateOnMount: false,
       suspense: true, // fallback to top-most suspense till load
-      // also as suspense data will never be null -> https://swr.vercel.app/docs/suspense
     }
   );
 
   const svgRef = useRef(null);
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    const topology = topojson.feature(geoData, geoData.objects['india-states']);
-    const projection = d3.geoMercator().fitSize([432, 488], topology);
-    const path = d3.geoPath(projection);
+    if (!geoData || !data) return;
+    const topology = feature(geoData, geoData.objects.states);
+    const projection = geoMercator().fitSize([432, 488], topology);
+    const path = geoPath(projection);
 
-    const features = topology.features.map((f: any) => {
-      const { district } = f.properties as { district: string };
-      const state = f.properties.st_nm as string;
-      const obj = { ...f };
-      obj.id = `TT-${state}${district ? `-${district}` : ''}`;
-      return obj;
+    const features = topology.features.map((f) => {
+      const { district } = f.properties;
+      const state = f.properties.st_nm;
+      const id = `TT-${state}${district ? `-${district}` : ''}`;
+      const value = 0;
+      return { ...f, ...{ id, value } };
     });
 
-    const svg = d3.select(svgRef.current);
+    const svg = select(svgRef.current);
     svg
       .select('.map')
       .selectAll('path')
-      .data(features, (d: any) => d.id)
+      .data(features)
       .enter()
       .append('path')
-      .attr('d', path as unknown as string)
+      .attr('d', path) // path is a function that takes a GeoJSON feature and returns a path string
       .attr('stroke-width', 0.5)
       .attr('stroke-opacity', 0.5)
       // .attr('fill', 'var(--dark-m-gray)')
@@ -73,7 +72,7 @@ const MapVisualizer = () => {
       .map((d) => {
         const { st_nm: stateCode } = d.properties;
         const stateData = data[STATE_CODES[stateCode]];
-        const value = Math.sqrt(stateData.total?.confirmed / 16);
+        const value = Math.sqrt((stateData.total?.confirmed ?? 0) / 16);
         d.value = value;
         return d;
       })
@@ -82,10 +81,17 @@ const MapVisualizer = () => {
     svg
       .select('.circle')
       .selectAll('circle')
-      .data(circlesData, (d) => d.id)
+      .data(circlesData)
       .enter()
       .append('circle')
-      .attr('transform', (d) => `translate(${path.centroid(d)})`)
+      .attr(
+        'transform',
+        (d) =>
+          `translate(${path
+            .centroid(d)
+            .map((n) => `${n}`)
+            .join(',')})`
+      )
       .attr('fill-opacity', 0.1)
       .attr('stroke-width', 0.4)
       .attr('r', (d) => Math.sqrt(d.value))
@@ -93,10 +99,10 @@ const MapVisualizer = () => {
       .attr('stroke', '#007bff')
       .style('cursor', 'pointer')
       .on('mouseenter', function (_) {
-        d3.select(this).attr('fill-opacity', 0.5);
+        select(this).attr('fill-opacity', 0.5);
       })
       .on('mouseleave', function (_) {
-        d3.select(this).attr('fill-opacity', 0.1);
+        select(this).attr('fill-opacity', 0.1);
       });
   });
   return (
